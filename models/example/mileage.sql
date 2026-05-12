39df72b5b99b1a133c2aa90e30caf2a6dc80e899
Neil Hanes <neil.hanes@wearemercury.com>
2026-05-12 14:50:33 +0100

    First Model

---
+++ models/example/mileage.sql
@@ -0,0 +1,24 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+with source_data as (
+
+    select 
+        PARSE_DATE( '%d.%m.%y', column1 ) as mileage_date,
+        column2                 as mileage,
+        column3                 as mileage_diff,
+        Notes                   as remarks
+    from
+        `mileage-495412.MileageData.Mileage`
+)
+
+select * from source_data
+
+
+
